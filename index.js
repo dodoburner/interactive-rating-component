9507e0925f4b7e5622194e9d2434ba6e1d55d1ad
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-04-12 16:30:42 +0200

    finished

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 let numbers= document.querySelectorAll('.number');
 let value;
 
-
 numbers.forEach(number => number.addEventListener('click', e => {
+  numbers.forEach(number => {
+    number.removeAttribute('id')});
   value= number.textContent;
-
+  number.setAttribute('id', 'value-selected')
 }));
 
 let submit= document.querySelector('#submit');
@@ -17,29 +18,33 @@ function removeAllChildNodes(parent) {
 };
 
 submit.addEventListener('click', e => {
-  removeAllChildNodes(boxOne);
+  if (value == undefined) {
+  } else {
+      removeAllChildNodes(boxOne);
   
-  let thankYouImg= document.createElement('img');
-  thankYouImg.setAttribute('id', 'thank-you-img');
-  thankYouImg.src="images/illustration-thank-you.svg";
+      let thankYouImg= document.createElement('img');
+      thankYouImg.setAttribute('id', 'thank-you-img');
+      thankYouImg.src="images/illustration-thank-you.svg";
 
-  let numberValue= document.createElement('p');
-  numberValue.textContent='You selected 4 out of 5'
-  numberValue.setAttribute('id', 'selected');
+      let numberValue= document.createElement('p');
+      numberValue.textContent='You selected ' + value + ' out of 5'
+      numberValue.setAttribute('id', 'selected');
 
-  let thankYou= document.createElement('p');
-  thankYou.textContent= 'Thank you!'
-  thankYou.setAttribute('id', 'thank-you');
+      let thankYou= document.createElement('p');
+      thankYou.textContent= 'Thank you!'
+      thankYou.setAttribute('id', 'thank-you');
 
-  let thankYouDesc= document.createElement('p');
-  thankYouDesc.setAttribute('id', 'thank-you-description');
-  thankYouDesc.textContent= 'We appreciate you taking the time to give a rating. If you ever need more support, don\'t hesitate to get in touch!'
+      let thankYouDesc= document.createElement('p');
+      thankYouDesc.setAttribute('id', 'thank-you-description');
+      thankYouDesc.textContent= 'We appreciate you taking the time to give a rating. If you ever need more support, don\'t hesitate to get in touch!'
 
-  boxOne.appendChild(thankYouImg);
-  boxOne.appendChild(numberValue);
-  boxOne.appendChild(thankYou);
-  boxOne.appendChild(thankYouDesc);
-  boxOne.setAttribute('id', 'box2')
+      boxOne.appendChild(thankYouImg);
+      boxOne.appendChild(numberValue);
+      boxOne.appendChild(thankYou);
+      boxOne.appendChild(thankYouDesc);
+      boxOne.setAttribute('id', 'box2')
+  }
+  
 
 })
 
